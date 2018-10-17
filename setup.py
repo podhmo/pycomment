1859c26cae44071003100b0deb0827916d4e100d
podhmo <ababjam61+github@gmail.com>
2018-10-17 12:19:58 +0900

    init

---
+++ setup.py
@@ -0,0 +1,47 @@
+import os
+import sys
+
+from setuptools import setup, find_packages
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    with open(os.path.join(here, 'README.rst')) as f:
+        README = f.read()
+    with open(os.path.join(here, 'CHANGES.txt')) as f:
+        CHANGES = f.read()
+except IOError:
+    README = CHANGES = ''
+
+install_requires = []
+
+docs_extras = []
+
+tests_require = []
+
+testing_extras = tests_require + []
+
+setup(
+    name='pycomment',
+    version='0.0.0',
+    description='with repr value at comment',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+    keywords='',
+    author="",
+    author_email="",
+    url="",
+    packages=find_packages(exclude=["pycomment.tests"]),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    extras_require={
+        'testing': testing_extras,
+        'docs': docs_extras,
+    },
+    tests_require=tests_require,
+    test_suite="pycomment.tests",
+    entry_points="""
+"""
+)
