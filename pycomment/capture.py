48f996057c427ccf9209303b3fc8f614ffbb8752
podhmo <ababjam61+github@gmail.com>
2019-10-12 06:34:27 +0900

    hmm

--- pycomment/capture.py
+++ pycomment/capture.py
@@ -7,25 +7,26 @@ from . import SEP_MARKER
 CaptureResult = namedtuple("CaptureResult", "comments, stdout")
 
 
-def _exec_self(code: str, *, g: t.Optional[dict] = None) -> None:
+def _exec_self(code: str, *, g: t.Optional[dict] = None) -> dict:
     g = g or {"__name__": "exec"}
     exec(code, g)
+    return g
 
 
-def _exec_in_tempfile(code: str, *, g: t.Optional[dict] = None) -> None:
+def _exec_in_tempfile(code: str, *, g: t.Optional[dict] = None) -> dict:
     import runpy
     import tempfile
 
     with tempfile.NamedTemporaryFile("w+", suffix=".py") as f:
         print(code, file=f)
         f.seek(0)
-        runpy.run_path(f.name, init_globals=g)
+        return runpy.run_path(f.name, init_globals=g)
 
 
 def capture(code: str, *, g: t.Optional[dict] = None, _exec=_exec_in_tempfile):
     o = StringIO()
     with contextlib.redirect_stdout(o):
-        _exec(code, g=g)
+        g = _exec(code, g=g)
 
     result_map = {}
     rest = []
