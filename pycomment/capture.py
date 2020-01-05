b2063a706eb090decb3ffa3f9db5d72a41cd2203
podhmo <ababjam61+github@gmail.com>
2020-01-05 20:20:19 +0900

    fix: run by same directory, even runpy

--- pycomment/capture.py
+++ pycomment/capture.py
@@ -7,26 +7,44 @@ from . import SEP_MARKER
 CaptureResult = namedtuple("CaptureResult", "comments, stdout")
 
 
-def _exec_self(code: str, *, g: t.Optional[dict] = None) -> dict:
+def _exec_self(
+    code: str, *, g: t.Optional[dict] = None, filename: t.Optional[str] = None,
+) -> dict:
     g = g or {"__name__": "exec"}
     exec(code, g)
     return g
 
 
-def _exec_in_tempfile(code: str, *, g: t.Optional[dict] = None) -> dict:
+def _exec_in_tempfile(
+    code: str, *, g: t.Optional[dict] = None, filename: t.Optional[str] = None,
+) -> dict:
     import runpy
     import tempfile
+    import os.path
+    import sys
 
-    with tempfile.NamedTemporaryFile("w+", suffix=".py") as f:
-        print(code, file=f)
-        f.seek(0)
-        return runpy.run_path(f.name, init_globals=g)
+    with contextlib.ExitStack() as s:
+        dirpath = None
+        if filename is not None:
+            dirpath = os.path.dirname(os.path.abspath(filename))
+            sys.path.insert(0, dirpath)
+            s.callback(lambda: sys.path.pop(0))
+        with tempfile.NamedTemporaryFile("w+", suffix=".py", dir=dirpath) as f:
+            print(code, file=f)
+            f.seek(0)
+            return runpy.run_path(f.name, init_globals=g)
 
 
-def capture(code: str, *, g: t.Optional[dict] = None, _exec=_exec_in_tempfile):
+def capture(
+    code: str,
+    *,
+    g: t.Optional[dict] = None,
+    filename: t.Optional[str] = None,
+    _exec=_exec_in_tempfile,
+):
     o = StringIO()
     with contextlib.redirect_stdout(o):
-        g = _exec(code, g=g)
+        g = _exec(code, g=g, filename=filename)
 
     result_map = {}
     rest = []
